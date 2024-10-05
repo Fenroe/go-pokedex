9582135ab50379334be202d279131f0bb05ae161
fenroe <scrambles9494@gmail.com>
2024-10-05 01:51:53 +0100

    Finished pokedex

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
-	"fmt"
+	"time"
+
+	"github.com/Fenroe/go-pokedex/internal/pokeapi"
 )
 
 func main() {
-	fmt.Println("Hi Banana!")
+	pokeClient := pokeapi.NewClient(5*time.Second, 5*time.Minute)
+	cfg := &config{
+		pokeapiClient: pokeClient,
+		caughtPokemon: map[string]pokeapi.Pokemon{},
+	}
+
+	startRepl(cfg)
 }
